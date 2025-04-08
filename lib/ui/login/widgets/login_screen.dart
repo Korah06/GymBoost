8cd6d2d9e1ac96ded1ad2cfa6471cafa19a17856
Mario Air <mario.espasa.2003@gmail.com>
2025-04-08 21:57:16 +0200

    Starting first glass component

--- lib/ui/login/widgets/login_screen.dart
+++ lib/ui/login/widgets/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gym_boost/data/model/login_data.dart';
 import 'package:gym_boost/ui/login/view_model/login_view_model.dart';
+import 'package:gym_boost/ui/widgets/glass/glass_card.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key, required this.viewModel});
@@ -26,40 +27,41 @@ class LoginScreen extends StatelessWidget {
                 child: CircularProgressIndicator(),
               );
             }
-            return Column(
+            return const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    elevation: 15,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Form(
-                        child: Column(
-                          children: [
-                            TextField(
-                              decoration: const InputDecoration(
-                                  labelText: 'Email',
-                                  hintText: 'email@example.com'),
-                              controller: emailController,
-                            ),
-                            TextField(
-                              decoration:
-                                  const InputDecoration(labelText: 'Password'),
-                              controller: passwordController,
-                            ),
-                            ElevatedButton(
-                              onPressed: () =>
-                                  viewModel.login.execute(loginData),
-                              child: const Text('Login'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
+                GlassCard(color: Colors.blue,),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Card(
+                //     elevation: 15,
+                //     child: Padding(
+                //       padding: const EdgeInsets.all(16),
+                //       child: Form(
+                //         child: Column(
+                //           children: [
+                //             TextField(
+                //               decoration: const InputDecoration(
+                //                   labelText: 'Email',
+                //                   hintText: 'email@example.com'),
+                //               controller: emailController,
+                //             ),
+                //             TextField(
+                //               decoration:
+                //                   const InputDecoration(labelText: 'Password'),
+                //               controller: passwordController,
+                //             ),
+                //             ElevatedButton(
+                //               onPressed: () =>
+                //                   viewModel.login.execute(loginData),
+                //               child: const Text('Login'),
+                //             ),
+                //           ],
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // )
               ],
             );
           },
---
+++ lib/ui/widgets/glass/glass_card.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlassCard extends StatelessWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+  final Color color;
+  final Widget? child;
+
+  const GlassCard({
+    super.key,
+    this.width = 200,
+    this.height = 200,
+    this.color = Colors.white,
+    this.borderRadius = 20,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final BorderRadius border = BorderRadius.circular(borderRadius);
+    return ClipRRect(
+      borderRadius: border,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: Stack(
+          children: [
+            //blur effect
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(),
+            ),
+            //gradient Effect
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: color.withValues(alpha: 0.15),
+                  width: 1,
+                ),
+                borderRadius: border,
+                gradient: LinearGradient(
+                  colors: [
+                    color.withValues(alpha: 0.5),
+                    color.withValues(alpha: 0.2),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+
+            //CHILDREN
+            child ?? const SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
